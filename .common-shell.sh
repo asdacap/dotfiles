4e9a691feac85aa7441e0c666f1ce590e7c574b5
Amirul Ashraf <asdacap@gmail.com>
2020-09-09 14:52:27 +0100

    Adding common shell script

---
+++ .common-shell.sh
@@ -0,0 +1,8 @@
+
+alias edt=$EDITOR
+alias edtzsh="edt ~/.zshrc && source ~/.zshrc"
+alias edtcommon="edt ~/.common-shell.sh && source ~/.zshrc"
+alias edttmux="edt ~/.tmux.conf"
+alias gs="git status"
+alias gca="git commit all"
+alias gcaa="git commit --amend all"
--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+[ ! -f "$HOME/.common-shell.sh" ] && ln -s "$PWD/.common-shell.sh" "$HOME/.common-shell.sh"
+
+LINE="source $HOME/.common-shell.sh"
+FILE="$HOME/.zshrc"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+FILE="$HOME/.bashrc"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+[ ! -f "$HOME/.ideavimrc" ] && ln -s "$PWD/.ideavimrc" "$HOME/.ideavimrc"
+
+# Vim setups
 mkdir -p $HOME/.config/
 [ ! -d "$HOME/.config/nvim" ] && ln -s "$PWD/nvim" "$HOME/.config/nvim"
 
@@ -10,5 +21,3 @@ mkdir -p $HOME/.config/coc/extensions/
 pushd $HOME/.config/coc/extensions/
 npm install
 popd
-
-[ ! -f "$HOME/.ideavimrc" ] && ln -s "$PWD/.ideavimrc" "$HOME/.ideavimrc"
