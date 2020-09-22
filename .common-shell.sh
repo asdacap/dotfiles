d6315008bb2d6331fade3802092d142ca4f39e95
Amirul Ashraf <ashamiru@amazon.com>
2020-09-22 11:23:20 +0100

    Updated some options

--- .common-shell.sh
+++ .common-shell.sh
@@ -4,5 +4,15 @@ alias edtzsh="edt ~/.zshrc && source ~/.zshrc"
 alias edtcommon="edt ~/.common-shell.sh && source ~/.zshrc"
 alias edttmux="edt ~/.tmux.conf"
 alias gs="git status"
+alias gd="git diff"
+alias gdh="git diff HEAD~"
 alias gca="git commit all"
 alias gcaa="git commit --amend all"
+alias tls="tmux ls"
+alias ta="tmux a -t"
+alias tn="tmux new -s"
+alias ffzf="find -f . | fzf"
+
+function rgfzf() {
+    rg -i $1 . | fzf ${@:2}
+}
