5755f9b071508db77b48fc2ec86182ea115f5f17
Amirul Ashraf <asdacap@gmail.com>
2020-10-04 22:21:27 +0100

    Adding yabai and skhdrc

--- setup.sh
+++ setup.sh
@@ -2,17 +2,21 @@
 
 set -e
 
+# Some links
 [ ! -f "$HOME/.common-shell.sh" ] && ln -s "$PWD/.common-shell.sh" "$HOME/.common-shell.sh"
+[ ! -f "$HOME/.yabairc" ] && ln -s "$PWD/.yabairc" "$HOME/.yabairc"
+[ ! -f "$HOME/.skhdrc" ] && ln -s "$PWD/.skhdrc" "$HOME/.skhdrc"
+[ ! -f "$HOME/.tmux.conf" ] && ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
 
+# Making sure stuff gets loaded
 LINE="source $HOME/.common-shell.sh"
 FILE="$HOME/.zshrc"
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 FILE="$HOME/.bashrc"
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
-[ ! -f "$HOME/.ideavimrc" ] && ln -s "$PWD/.ideavimrc" "$HOME/.ideavimrc"
-
 # Vim setups
+[ ! -f "$HOME/.ideavimrc" ] && ln -s "$PWD/.ideavimrc" "$HOME/.ideavimrc"
 mkdir -p $HOME/.config/
 [ ! -d "$HOME/.config/nvim" ] && ln -s "$PWD/nvim" "$HOME/.config/nvim"
 
@@ -21,3 +25,5 @@ mkdir -p $HOME/.config/coc/extensions/
 pushd $HOME/.config/coc/extensions/
 npm install
 popd
+
+
